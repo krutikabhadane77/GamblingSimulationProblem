4064a8fa6ca473ed1090d4b9cda6cc43b2d0e2e8
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-21 21:58:41 +0530

    Added: UC1 As a gambler,would start with a stake of  every day and bet  every game

---
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,11 @@
+//UC1 As a gambler,would start with a stake of $100 every day and bet $1 every game.
+package com.gamblingsimulation;
+
+public class GamblingSimulation {
+    public static void main(String[] args){
+        System.out.println("Welcome to Gambling Simulation Problem");
+        int bet=1;
+        int stakePerDay=100;
+        System.out.println("stake per day of gambler is "+stakePerDay+" and bet is "+bet);
+    }
+}
