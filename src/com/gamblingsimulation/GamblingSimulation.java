221454d8b4133303dd286ae761ceb1f3db3b7181
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-23 14:38:19 +0530

    Added: UC3 As a cumulative gambler if won or lost 50% stake, woyld resign for the day

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,31 +1,49 @@
 //UC1 As a gambler,would start with a stake of $100 every day and bet $1 every game.
 //UC2 As a gambler make $1 bet so either win or loose $1.
+//UC3 As a calculative gambler if won or lost 50% of the stake, would resign for the day.
 package com.gamblingsimulation;
 import java.lang.Math;
 import java.util.Random;
 
 public class GamblingSimulation {
-    final int bet=1;
-    final int stakePerDay=100;
-    public static void main(String[] args){
-        GamblingSimulation gambling=new GamblingSimulation();
+    final int bet = 1;
+    final int stake = 100;
+    final int wins = 0;
+    final int losses = 0;
+    final int count = 0;
+
+
+    public static void main(String[] args) {
+        GamblingSimulation gambling = new GamblingSimulation();
         System.out.println("Welcome to Gambling Simulation Problem");
-        System.out.println("stake per day of gambler is "+gambling.stakePerDay+" and bet is "+gambling.bet);
-        playGame(gambling);
+        System.out.println("stake per day of gambler is " + gambling.stake + " and bet is " + gambling.bet);
+        playGame(gambling, gambling.bet, gambling.stake, gambling.wins, gambling.losses);
     }
 
-    public static void playGame(GamblingSimulation gambling){
-        Random random=new Random();
-        int stake=0;
-        int bet=random.nextInt(2);
-        if (bet==1) {
-            System.out.println("Gambler won the bet");
-            stake += gambling.bet;
+
+    public static void playGame(GamblingSimulation gambling, int bet, int stake, int wins, int looses) {
+        while(stake!=0){
+            Random random = new Random();
+            int game = random.nextInt(2);
+            if (game == 0) {
+                stake -= gambling.bet;
+                looses += 1;
+                System.out.println("Gambler loose the bet");
+                if (looses == 50) {
+                    System.out.println("Gambler reached max losses");
+                    break;
+                }
+            } else {
+                stake += gambling.bet;
+                System.out.println("Gambler won the bet");
+                wins += 1;
+                if (wins == 50) {
+                    System.out.println("Gambler reached max wins");
+                    break;
+                }
+            }
         }
-        else
-        {
-            stake -= gambling.bet;
-            System.out.println("Gambler loose the bet");
-        }
+        System.out.println("Total wins "+wins);
+        System.out.println("Total looses "+looses);
     }
 }
